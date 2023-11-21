8416912c76e94e1a3ed89d824688caa5cc24fb67
Jouw Naam <studentnummer@ma-web.nl>
2023-11-21 12:23:26 +0100

    .

--- 01A/app.js
+++ 01A/app.js
@@ -2,7 +2,24 @@ class App
 {
     runApplication(){
         console.log("hallo allemaal, wat fijn dat je er bent, ben je voor het eerst hier of ben je al bekend, stamp met je voeten doe je handen in je saai, ik ben ANK en wie ben jij ");
-    }
+        
+let appnaam = "Angrybirds"
+let versienummer = "0,4"
+let autheur = "Paul van Loon"
+let copyright = "codeertaal"
+let distributeur = "Apple"
+let darkmode = "true"
+
+console.log(appnaam)
+console.log(versienummer)
+console.log(autheur)
+console.log(copyright)
+console.log(distributeur)
+console.log(darkmode)
+    
+
+}
 }
 let app = new App();
 app.runApplication();
+
