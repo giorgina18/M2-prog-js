919cfa6f82c173ec01d65acf78bbce104103cdd4
Jouw Naam <studentnummer@ma-web.nl>
2023-11-21 13:12:56 +0100

    opdracht_4

---
+++ 01C/app.js
@@ -0,0 +1,41 @@
+class App
+{
+    runApplication(){
+        
+        console.log("hallo allemaal, wat fijn dat je er bent, ben je voor het eerst hier of ben je al bekend, stamp met je voeten doe je handen in je saai, ik ben ANK en wie ben jij ");
+        this.greeting = "starting up"
+this.appnaam = "Angrybirds"
+this.versienummer = "0,4"
+this.autheur = "Paul van Loon"
+this.copyright = "codeertaal"
+this.distributeur = "Apple"
+this.darkmode = "true"
+this.lightmode ="false"
+this.nummer ="10"
+this.woord= "hallo"
+let vegatarish ="false"
+let number ="25"
+let woorden= "appelmoes"
+console.log(vegatarish)
+console.log(number)
+console.log(woorden)
+
+
+}
+}
+let app = new App();
+app.runApplication();
+console.log(app.greeting);
+console.log("appNaam: "+ app.appNaam );
+console.log("versienummer:" + app.versienummer);
+
+console.log(app.appnaam)
+console.log(app.versienummer)
+console.log(app.autheur)
+console.log(app.copyright)
+console.log(app.distributeur)
+console.log(app.darkmode)
+console.log(app.lightmode)
+console.log(app.nummer)
+console.log(app.woord)
+    
