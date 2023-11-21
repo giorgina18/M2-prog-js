4155020a2f8135f434d5411a49b0c758ad43f4ee
Jouw Naam <studentnummer@ma-web.nl>
2023-11-21 11:57:24 +0100

    .

---
+++ 01A/app.js
@@ -0,0 +1,8 @@
+class App
+{
+    runApplication(){
+        console.log("hallo allemaal, wat fijn dat je er bent, ben je voor het eerst hier of ben je al bekend, stamp met je voeten doe je handen in je saai, ik ben ANK en wie ben jij ");
+    }
+}
+let app = new App();
+app.runApplication();
