ed8a8fecab00142fa8def90f52f270c3a4d77c0e
Jouw Naam <studentnummer@ma-web.nl>
2023-11-23 12:19:45 +0100

    opdracht_canvas2

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -1,13 +1,17 @@
 class App
 {
     runApplication(){
-        
+
 console.log("hallo wereld!")        
 let canvas = document.getElementById("canvasId")
 console.log(canvas)
 let g = canvas.getContext("2d");
+g.fillStyle = "blue";
 g.fillRect(0,0,10,10);
+g.fillRect(0,0,canvas.width,canvas.height);
 
+g.fillStyle = "lightblue"; 
+g.fillRect(235,150,100,100)
 
 }
 }
