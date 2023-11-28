682170d8aeeb581ba147f573f41722b3f488189c
Jouw Naam <studentnummer@ma-web.nl>
2023-11-28 11:52:21 +0100

    nieuws

---
+++ 03 ifelse/app.js
@@ -0,0 +1,19 @@
+class App {
+    runApplication() {
+
+        console.log("hellow world")
+        let title = document.getElementById("newstitle")
+        let headline = document.getElementById("gamenews headline")
+        let gamenews = document.getElementById("gamenews")
+
+        console.log(title)
+        console.log(headline)
+        console.log(gamenews)
+
+
+
+    }
+}
+let app = new App();
+app.runApplication();
+
