4a2b8319e8a78a2c528de7c1e77868ff16fe9485
Jouw Naam <studentnummer@ma-web.nl>
2023-11-28 13:00:17 +0100

    kleur_tekst

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -1,19 +1,57 @@
 class App {
     runApplication() {
 
-        console.log("hellow world")
-        let title = document.getElementById("newstitle")
-        let headline = document.getElementById("gamenews headline")
-        let gamenews = document.getElementById("gamenews")
+        console.log("hello world");
 
+        let title = document.getElementById("newstitle")
         console.log(title)
+
+        let headline = document.getElementsByClassName("headline")
         console.log(headline)
+
+        let gamenews = document.getElementsByClassName("gamenews")
         console.log(gamenews)
 
+
+        let random = Math.random();//0-1.000
+        console.log(random)
+
+        if (random >= 0 && random < 0.2) {
+            title.style.backgroundColor = "#FF0000";
+        }
+        else if (random >= 0.2 && random < 0.6) {
+            title.style.backgroundColor = "#00FF00";
+        }
+        else if (random > 0.2 && random < 0.75) {
+            title.style.backgroundColor = "#9933FF";
+        }
+        else if (random > 0.75) {
+            title.style.backgroundColor = "#FF0099";
+        }
+
+
+        let newsitem1 = document.getElementsByClassName("gamenews")[0]
+        let newsitem2 = document.getElementsByClassName("gamenews")[1]
+
+
+        if (random >= 0 && random < 0.2) {
+            newsitem1.style.backgroundColor = "#00FF00";
+        }
+        else if (random >= 0.2 && random < 0.6) {
+            newsitem1.style.backgroundColor = "#FF0000";
+        }
+
+        if (random > 0.2 && random < 0.75) {
+            newsitem2.style.backgroundColor = "#FF0099";
+        }
+        else if (random > 0.75) {
+            newsitem2.style.backgroundColor = "#9933FF";
+        }
 
 
     }
 }
 let app = new App();
 app.runApplication();
+
 
