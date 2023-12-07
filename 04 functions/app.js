2f3829722eef716d37140d626ae1a6fd51399982
Jouw Naam <studentnummer@ma-web.nl>
2023-12-07 13:10:22 +0100

    functions

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,117 +1,139 @@
 
-// function heeftEenResultaat() {
-//     let resulaat = 1;
+function heeftEenResultaat() {
+    let resulaat = 1;
 
-//     return resulaat
-// }
-// let hetResultaat = heeftEenResultaat();
-// console.log(hetResultaat);
-// console.log(heeftEenResultaat());
-
-
-
-// let x = 9;
-// let a = 3;
-// let b = 4;
-// let c = 89;
-
-
-// function ax2bcWiskunde(x, a, b, c) {
-
-//     let y = (a * (x * x)) + (b * x) + c;
-//     console.log(y);
-
-//     return y;
-// }
-// let y1 = ax2bcWiskunde(9, 3, 4, 89);
-// console.log(y1)
-
-// let y2 = ax2bcWiskunde(3, 6, 5, 45)
-// console.log(y2)
-
-// let y3 = ax2bcWiskunde(7, 1, 2, 13)
-// console.log(y3)
-
-
-// let u = 9;
-// let h = 8;
-// let l = 15;
-// //  let logY = Math.log(1) + h + Math.pow(u,2);
-// //  console.log(logY)
-// function wiskunde(u, h, l) {
+    return resulaat
+}
+let hetResultaat = heeftEenResultaat();
+console.log(hetResultaat);
+console.log(heeftEenResultaat());
 
 
 
-//     let logY = Math.log(l) + h + Math.pow(u, 2);
-//     console.log(logY)
-//     return logY;
-
-// }
-
-// let k = (u * (l * l)) + (h * u) + l
-// console.log(k)
-
-// let k1 = wiskunde(7, 5, 0.5, 22)
-// console.log(k1)
-
-// let k2 = wiskunde(3, 2, 56, 0.3)
-// console.log(k2)
-
-// let k3 = wiskunde(2, 67, 0.2, 4)
-// console.log(k3)
+let x = 9;
+let a = 3;
+let b = 4;
+let c = 89;
 
 
+function ax2bcWiskunde(x, a, b, c) {
+
+    let y = (a * (x * x)) + (b * x) + c;
+    console.log(y);
+
+    return y;
+}
+let y1 = ax2bcWiskunde(9, 3, 4, 89);
+console.log(y1)
+
+let y2 = ax2bcWiskunde(3, 6, 5, 45)
+console.log(y2)
+
+let y3 = ax2bcWiskunde(7, 1, 2, 13)
+console.log(y3)
+
+
+let u = 9;
+let h = 8;
+let l = 15;
+//  let logY = Math.log(1) + h + Math.pow(u,2);
+//  console.log(logY)
+function wiskunde(u, h, l) {
+
+
+
+    let logY = Math.log(l) + h + Math.pow(u, 2);
+    console.log(logY)
+    return logY;
+
+}
+
+let k = (u * (l * l)) + (h * u) + l
+console.log(k)
+
+let k1 = wiskunde(7, 5, 0.5, 22)
+console.log(k1)
+
+let k2 = wiskunde(3, 2, 56, 0.3)
+console.log(k2)
+
+let k3 = wiskunde(2, 67, 0.2, 4)
+console.log(k3)
 
 
 
 
-// function argumentsAreHandy(shoutout) {
-//     console.log("do you want to give a shout out")
-//     console.log(shoutout);
-// }
-// argumentsAreHandy("Super Shout out");
-// argumentsAreHandy("hou het netjes!");
-// argumentsAreHandy("je bent super goed bezig");
+
+
+function argumentsAreHandy(shoutout) {
+    console.log("do you want to give a shout out")
+    console.log(shoutout);
+}
+argumentsAreHandy("Super Shout out");
+argumentsAreHandy("hou het netjes!");
+argumentsAreHandy("je bent super goed bezig");
 
 
 
 
-// function superMooieGlobalFunction() {
-//     console.log("ik ben global");
-//     console.log("dus je mag mij overal aanroepen");
+function superMooieGlobalFunction() {
+    console.log("ik ben global");
+    console.log("dus je mag mij overal aanroepen");
 
-// }
+}
 
-// function aanroepVoorbeeld() {
+function aanroepVoorbeeld() {
 
-// }
+}
 
-// superMooieGlobalFunction();
-// aanroepVoorbeeld();
+superMooieGlobalFunction();
+aanroepVoorbeeld();
 
 
 class App {
-    runApplication() 
-    {
+    runApplication() {
         console.log("hello world!")
-       //superMooieGlobalFunction();
+        //superMooieGlobalFunction();
     }
-    newClassfunction()
-    {
+    newClassfunction() {
         console.log("hello world in de nieuwClassFunction");
     }
 
-    anotherfunctionWithArguments(aArgument)
-    {
-console.log("kom maar met je argument");
-console.log("hier:");
-console.log(aArgument);
+    anotherfunctionWithArguments(aArgument) {
+        console.log("kom maar met je argument");
+        console.log("hier:");
+        console.log(aArgument);
+    }
+    marioLuigi() {
+        console.log("MARIO!!")
+
+    }
+    browser() {
+        let browser = "BWAHAHAHA";
+        return browser;
+    }
+
+    getallen(o, p) {
+        
+    let O = Math.log(o) + p + Math.pow(o, p);
+    console.log(O)
+    return O;
+
     }
 }
-
-
 let app = new App();
+
+let o = app.getallen(88,2)
+console.log(o)
+let p = app.getallen(143, 0.5)
+console.log(p)
+
+
 app.runApplication();
 app.newClassfunction();
 app.anotherfunctionWithArguments("mijn argument is goed uitgelegd ");
+app.marioLuigi();
 
+let eindResultaat = app.browser();
+console.log(eindResultaat);
+console.log(app.browser());
