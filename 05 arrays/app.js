96f0128c03e51c02900c84be5e06a76040523fae
Jouw Naam <studentnummer@ma-web.nl>
2023-12-14 12:26:48 +0100

    add_remove

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -4,10 +4,15 @@ class App {
 
         let favorieten = ["skillet(1)", "viezeAsbak(2)", "dikkeBaap(3)", "vlinderVos(4)", "natteVisstick(5)"]
         console.log(favorieten)
+        favorieten.push("ladyyGaga")
+
+        let indexToRemove = favorieten.indexOf("ladyGaga")
+        favorieten.splice(indexToRemove, 1)
+        favorieten.push("XXXtentacion", "juiceWorld")
 
         for (let i = 0; i < favorieten.length; i++) {
             const favoriet = favorieten[i];
-            console.log(i + ":" +  favoriet )
+            console.log(i + ":" + favoriet)
         }
 
         let nummers = [0.5, 22, 980]
