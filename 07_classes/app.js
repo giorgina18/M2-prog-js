db2225d9bb842bce675740dee6202a286fa47d26
Jouw Naam <studentnummer@ma-web.nl>
2024-01-23 11:49:26 +0100

    classes

---
+++ 07_classes/app.js
@@ -0,0 +1,34 @@
+class Greet {
+    constructor() {
+        console.log("greetings!");
+    }
+
+}
+
+let greet = new Greet()
+let greet1 = new Greet()
+let greet2 = new Greet()
+let greet3 = new Greet()
+
+
+
+class GoodBye {
+    constructor() {
+        console.log("bye bye:)!");
+    }
+
+}
+
+let bye = new GoodBye()
+
+
+
+class App {
+    runApplication() {
+        console.log("hello world");
+
+    }
+}
+let app = new App();
+app.runApplication();
+
